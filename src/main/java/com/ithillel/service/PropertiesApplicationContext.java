d7241413cf44828be60fad26dfe26356f6ae4c04
ktvcv <kotovec.vii@gmail.com>
2020-10-13 21:47:34 +0300

    modified

--- src/main/java/com/ithillel/service/PropertiesApplicationContext.java
+++ src/main/java/com/ithillel/service/PropertiesApplicationContext.java
@@ -1,37 +1,86 @@
 package com.ithillel.service;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 public class PropertiesApplicationContext implements ApplicationContext {
 
-    private static final String TEXT_PROCESSOR = "textProcessor";
-    private Map<String, Object> beans = new HashMap<>();
+    private Map<String, Object> beanObjects = new HashMap<>();
 
     public PropertiesApplicationContext() {
-        Properties applicationProperties = new Properties();
-        try {
-            applicationProperties.load(getClass().getClassLoader().getResourceAsStream("application.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String value = applicationProperties.getProperty(TEXT_PROCESSOR);
-        try {
-            beans.put(TEXT_PROCESSOR, Class.forName(value).getDeclaredConstructor(Storage.class).newInstance(new HashMapStorage()));
-        } catch (Exception e) {
+        JSONParser jsonParser = new JSONParser();
+
+        try
+        {
+            JSONObject obj = (JSONObject) jsonParser.parse(new FileReader("C:\\Users\\Victoria\\IdeaProjects\\TextProcessorHW2.2\\src\\main\\resources\\application.json"));
+
+            JSONArray arrayBean = (JSONArray)obj.get("beans");
+
+            for (Object bean : arrayBean) {
+                buildBean((JSONObject) bean);
+            }
+
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
     }
 
+    private void buildBean(JSONObject bean)
+    {
+        String name = (String) bean.get("name");
+        String type = (String) bean.get("type");
+
+        JSONArray argsJSON = (JSONArray)bean.get("constructorArgs");
+
+        StringBuilder args = new StringBuilder();
+        if(argsJSON != null)
+            for (Object o: argsJSON) {
+                args.append(o);
+        }
+
+        buildBeanObject(name,type,args.toString());
+    }
+
+    private void buildBeanObject(String name, String type, String args) {
+        if (args.isEmpty())
+            try {
+                beanObjects.put(name, Class.forName(type).getDeclaredConstructor().newInstance());
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        else {
+            List<String> argsList = Arrays.asList(args.split(","));
+            argsList.forEach(e -> {
+                try {
+                    if (beanObjects.get(e) != null) {
+                        beanObjects.put(name, Class.forName(type).getDeclaredConstructor(Storage.class).newInstance(beanObjects.get(e)));
+                        //beanObjects.put(name, Class.forName(type).getConstructor(beanObjects.get(e).getClass().getInterfaces()[?]).newInstance(beanObjects.get(e)));
+                    }}
+                catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            });
+
+        }
+
+    }
+
+
     @Override
     public Object getBean(String name) {
-        return beans.get(name);
+        return beanObjects.get(name);
     }
 
     public static void main(String[] args) {
         PropertiesApplicationContext p = new PropertiesApplicationContext();
-
+        System.out.println(p.beanObjects);
     }
 }
