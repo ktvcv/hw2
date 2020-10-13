0ea43b3bf4ca615e9012a1740a1a55224268a09e
ktvcv <kotovec.vii@gmail.com>
2020-10-13 23:32:09 +0300

    refresh

--- src/main/java/com/ithillel/service/PropertiesApplicationContext.java
+++ src/main/java/com/ithillel/service/PropertiesApplicationContext.java
@@ -10,6 +10,7 @@ import java.util.*;
 public class PropertiesApplicationContext implements ApplicationContext {
 
     private Map<String, Object> beanObjects = new HashMap<>();
+
     public PropertiesApplicationContext() {
         Properties applicationProperties = new Properties();
         try {
@@ -17,36 +18,37 @@ public class PropertiesApplicationContext implements ApplicationContext {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for(int i = 0; i < 2; i++){
+        for (int i = 0; i < 2; i++) {
             buildBean(applicationProperties.getProperty("bean[" + i + "].name"),
-                      applicationProperties.getProperty("bean[" + i + "].type"),
-                      applicationProperties.getProperty("bean[" + i + "].args"));
+                    applicationProperties.getProperty("bean[" + i + "].type"),
+                    applicationProperties.getProperty("bean[" + i + "].args"));
         }
     }
 
     private void buildBean(String name, String type, String args) {
-            if (args == null)
-                try {
-                    beanObjects.put(name, Class.forName(type).getDeclaredConstructor().newInstance());
-                }
-                    catch (Exception e){
-        e.printStackTrace();
-    }
-            else {
-                List<String> argsList = Arrays.asList(args.split(","));
-                argsList.forEach(e -> {
-                            try {
-                                if (beanObjects.get(e) != null) {
-                                    beanObjects.put(name, Class.forName(type).getDeclaredConstructor(Storage.class).newInstance(beanObjects.get(e)));
-                                    //beanObjects.put(name, Class.forName(type).getConstructor(beanObjects.get(e).getClass().getInterfaces()[0]).newInstance(beanObjects.get(e)));
-                                 }}
-                            catch (Exception ex){
-                                ex.printStackTrace();
-                            }
-                        });
+        if (args == null)
+            try {
+                beanObjects.put(name, Class.forName(type).getDeclaredConstructor().newInstance());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
+        else {
+            List<String> argsList = Arrays.asList(args.split(","));
+            argsList.forEach(e -> {
+                try {
+                    if (beanObjects.get(e) != null) {
+                        beanObjects.put(name, Class.forName(type).getDeclaredConstructor(Storage.class).newInstance(beanObjects.get(e)));
+                        //beanObjects.put(name, Class.forName(type).getConstructor(beanObjects.get(e).getClass().getInterfaces()[0]).newInstance(beanObjects.get(e)));
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace();
                 }
+            });
+
+        }
     }
+
     @Override
     public Object getBean(String name) {
         return beanObjects.get(name);
